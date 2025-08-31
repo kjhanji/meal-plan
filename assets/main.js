47a4721eaa2ed811a5d136f5625dea2257bcf79e
Karan Jhanji <karanjhanji@hotmail.com>
2025-08-31 12:53:50 -0700

    add pop-up functionality for test mmodal

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,10 @@
-const add_button = document.querySelector('.add-button');
-const add_container = document.querySelector('.add-container');
-const add_close = document.querySelector('.add-close');
+const open = document.getElementById('open');
+const modal_container = document.getElementById('modal-container');
+const close = document.getElementById('close');
 
-
-add_button.addEventListener('click', () => {
-    add_container.classList.add('show');
-});
-
-add_close.addEventListener('click', () => {
-    add_container.classList.remove('show');
-});
+open.addEventListener('click', () => {
+    modal_container.classList.add('show');
+})
+close.addEventListener('click', () => {
+    modal_container.classList.remove('show');
+})
